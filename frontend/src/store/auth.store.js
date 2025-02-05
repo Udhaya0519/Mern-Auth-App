06c82b4a88ca12d6b26da02e391bdda5a6721c4d
Udhaya0519 <udhaya0519@gmail.com>
2025-02-05 10:57:49 +0530

    fixed ENVS

--- frontend/src/store/auth.store.js
+++ frontend/src/store/auth.store.js
@@ -2,7 +2,7 @@ import { create } from 'zustand'
 import axios from 'axios'
 import toast from 'react-hot-toast'
 
-const API_URL = "http://localhost:3000/api/auth"
+const API_URL = import.meta.env.mode === "development" ? "http://localhost:3000/api/auth" : "/api/auth"
 axios.defaults.withCredentials = true //axios add cookies to the header everytime request is made
 
 export const useAuthStore = create((set) => ({
