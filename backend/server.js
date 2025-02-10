fdca787b99e653880f602ea1fdcbe09175e08ae2
Udhaya0519 <udhaya0519@gmail.com>
2025-02-10 19:39:56 +0530

    set up for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cookieParser from 'cookie-parser';
 import cors from 'cors'
-import path from 'path'
+// import path from 'path'
 import dotenv from 'dotenv'
 
 import { connectDB } from './db/connectDB.js';
@@ -11,25 +11,27 @@ dotenv.config()
 
 
 const app = express();
-const PORT = 3000 
-const __dirname = path.resolve()
+// const __dirname = path.resolve()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({ origin:"http://localhost:5173", credentials:true}))
+app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173", credentials:true}))
 
 
 
 
 app.use('/api/auth', authRoutes)
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "/frontend/dist")))
-
-    app.get("*", (req,res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-    })
-}
+// if(process.env.NODE_ENV === "production"){
+    //     app.use(express.static(path.join(__dirname, "/frontend/dist")))
+    
+    //     app.get("*", (req,res) => {
+        //         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+        //     })
+        // }
+        
+        
+const PORT = process.env.PORT || 3000; 
 
 
 app.listen(PORT,() => {
