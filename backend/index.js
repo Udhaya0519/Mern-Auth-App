f2ef0bd6b0c4d53aef4ca518bc17477f0414837c
Udhaya0519 <udhaya0519@gmail.com>
2025-02-11 09:55:39 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 
-app.use(cors({ origin: process.env.CLIENT_URL, credentials:true}))
+app.use(cors({ origin: ["https://mern-auth-app-pi.vercel.app, https://mern-auth-app-udhaya-js-projects.vercel.app"], credentials:true}))
 
 
 
